ceda09db608a04eabc19a9aa49650112ac63666a
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2020-01-10 15:04:25 +0000

    Update sha1

--- HomebrewFormula/snipcli.rb
+++ HomebrewFormula/snipcli.rb
@@ -2,7 +2,7 @@ class Snipcli < Formula
   desc "Snipline CLI is the command-line tool for Snipline"
   homepage "https://snipline.io/"
 	url "https://github.com/snipline/snipcli/releases/download/0.3.0/snipcli.tar.gz"
-  sha256 "b3db863d2e8701275ad185afb1fc61be9f4edde48be436a6479726e36f1d4fa3"
+  sha256 "ed116a85011095d206e178130237f742575644c8ec436a3eeca8ef9d0eb219c4"
 
 	depends_on "crystal"
 	depends_on "readline" => :recommended
