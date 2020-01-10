8aa97b426ca425ae11cfa6757e2b9505cd58800a
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2020-01-10 14:34:56 +0000

    Possible fix for installing with binary

--- HomebrewFormula/snipcli.rb
+++ HomebrewFormula/snipcli.rb
@@ -1,14 +1,14 @@
 class Snipcli < Formula
   desc "Snipline CLI is the command-line tool for Snipline"
   homepage "https://snipline.io/"
-  url "https://github.com/snipline/snipcli/archive/0.3.0.tar.gz"
-  sha256 "fda5337103d5f19c909b926167546b82f8d9936b9785c5953faccb91d9ddce95"
+	url "https://github.com/snipline/snipcli/releases/download/0.3.0/snipcli_macos.tar.gz"
+  sha256 "080b93778797eb4c77b641c06a65f6e19791e89a208a351f2535dcb80c3333e1"
 
 	depends_on "crystal"
 
   def install
-    system "shards", "install"
-    system "make", "install", "PREFIX=#{prefix}"
+    # system "shards", "install"
+    # system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
