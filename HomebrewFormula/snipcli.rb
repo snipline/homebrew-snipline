817f1029b08c36bfdacf41349726e86c7ca1a2fa
Mitchell Stanley <mitchell.stanley@isev.co.uk>
2020-01-10 14:02:10 +0000

    Update to 0.3.0

--- HomebrewFormula/snipcli.rb
+++ HomebrewFormula/snipcli.rb
@@ -1,10 +1,10 @@
 class Snipcli < Formula
   desc "Snipline CLI is the command-line tool for Snipline"
   homepage "https://snipline.io/"
-  url "https://github.com/snipline/snipcli/archive/0.2.0.tar.gz"
-  sha256 "d1a67db86b769d0cf3ec9e0e96bb653683c67518d1c1212426c06572b1bd0d63"
+  url "https://github.com/snipline/snipcli/archive/0.3.0.tar.gz"
+  sha256 "fda5337103d5f19c909b926167546b82f8d9936b9785c5953faccb91d9ddce95"
 
-  depends_on "crystal"
+	depends_on "crystal@0.31.1"
 
   def install
     system "shards", "install"
@@ -13,6 +13,6 @@ class Snipcli < Formula
 
   test do
     output = shell_output("#{bin}/snipcli --version")
-    assert_match "0.2.0", output
+    assert_match "0.3.0", output
   end
 end
